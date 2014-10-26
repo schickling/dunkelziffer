8dff7bce8edff405cad85a37007e609021f2a71a
Johannes Schickling <schickling.j@gmail.com>
2014-10-26 13:18:33 +0100

    fix

--- server/provider/data/kavkaz.js
+++ server/provider/data/kavkaz.js
@@ -15,7 +15,7 @@ module.exports = {
     parse: function($) {
 
         var title = $('h1').html();
-        var text = htmlToText($('#text-b').html()).replace(/\[.*\]/g, '');
+        var text = htmlToText($('#text-b').html().replace(/&#xFFFD;/g, '').replace(' <br />', '')).replace(/\[.*\]/g, '');
 
         return {
             title: title,
