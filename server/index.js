c2debcc03670082146822e51cf0635aa701bf09e
Johannes Schickling <schickling.j@gmail.com>
2014-10-26 08:41:25 +0100

    fix keyword issue

--- server/index.js
+++ server/index.js
@@ -5,27 +5,17 @@ var io = require('socket.io')(server);
 var crawler = require('./crawler');
 var readdir = require('fs').readdirSync;
 
-var lastKeyword;
-
 io.on('connection', function(socket) {
 
     socket.on('keyword', function(keyword) {
         var currentKeyword = keyword;
-
-        if (lastKeyword !== currentKeyword) {
-            lastKeyword = currentKeyword;
-
-            readdir(__dirname + '/provider').forEach(function(file) {
-                var provider = require('./provider/' + file);
-
-                crawler(currentKeyword, provider).then(function(data) {
-                    if (lastKeyword === currentKeyword) {
-                        data.keyword = currentKeyword;
-                        socket.emit('data', data);
-                    }
-                });
+        readdir(__dirname + '/provider').forEach(function(file) {
+            var provider = require('./provider/' + file);
+            crawler(currentKeyword, provider).then(function(data) {
+                data.keyword = currentKeyword;
+                socket.emit('data', data);
             });
-        }
+        });
     });
 
 });
