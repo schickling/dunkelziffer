c5df567d3fbbc7ddc58533ae4cf92c8d7fa81575
Anderson Veiga <preams.anderson@gmail.com>
2014-10-26 11:50:18 +0100

    Show search results in html

--- public/script/app.js
+++ public/script/app.js
@@ -95,33 +95,32 @@
 $(document).ready(function(){
 
     var socket = io(),
-        results = [],
+        lastKeyword = '',
+        resultsDeep = resultsClear = [],
         _isSearching = false,
         isSearching = function(value){
-            console.log('called', value);
             if(typeof value !== 'undefined'){
                 _isSearching = value;
             }
 
-            setTimeout(function(){
-                //toggle search indicator
-                if(_isSearching){
-                    $('#search-indicator').show();
-                } else {
-                    console.log('should hide');
-                    $('#search-indicator').css('color','red');
-                }
-            },0);
+            //toggle search indicator
+            if(_isSearching){
+                $('#search-indicator').show();
+            } else {
+                $('#search-indicator').hide();
+            }
 
             return _isSearching;
         },
-        searchTimeoutThrottle = 300,
+        searchTimeoutThrottle = 500,
         searchTimeout;
 
     socket.on('data', function(data) {
-        console.log('search results', data);
-        isSearching(false);
+        populateResults(data);
     });
+
+    socket.emit('keyword', 'russia');
+
 
     $('#query').on('keyup', function(){
         clearTimeout(searchTimeout);
@@ -129,7 +128,7 @@ $(document).ready(function(){
 
         // validate search query
         if(!query){
-            results = []; //reset results if query is empty
+            populateResults(); //reset results if query is empty
             return;
         }
 
@@ -138,10 +137,58 @@ $(document).ready(function(){
         },searchTimeoutThrottle);
     });
 
+    var populateResults = function(results){
+
+        if(!results){
+            resetSearch();
+            return;
+        }
+
+        if(results.keyword!==lastKeyword){
+            return;
+        }
+
+        if(results.isDeep){
+            resultsDark = Array.prototype.concat(resultsDark, results.data);
+        } else {
+            resultsClear = Array.prototype.concat(resultsClear, results.data);
+        }
+
+        var resultsClearHtml = resultsDarkHtml = '';
+
+        resultsClear.forEach(function(result){
+            resultsClearHtml += resultTemplate(result);
+        });
+
+        $('#results-clear').html(resultsClearHtml);
+        $('#results-dark').html(resultsDarkHtml);
+
+        isSearching(false);
+    };
+
+    var resultTemplate = function(result){
+        var template = $($('#result-template').html());
+
+
+        template.attr('href', result.url);
+        template.find('.result-title').html(result.title);
+        template.find('.result-source').html(result.source);
+        template.find('.result-date').html(result.date);
+
+        return template[0].outerHTML;
+    };
+
+    var resetSearch = function(){
+        resultsClear = resultsDark = [];
+        $('#results-clear, #results-dark').html('');
+    };
 
     var doSearch = function(query){
-        console.log('do search', query, isSearching());
         if(!isSearching()){
+            if(lastKeyword!==query){
+                resetSearch();
+            }
+            lastKeyword = query;
             isSearching(true);
             socket.emit('keyword', query);
         }
